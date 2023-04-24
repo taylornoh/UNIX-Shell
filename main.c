01aab73b7bd908dcfd28d5294313a7e9c9fdbdb3
taylornoh <taylornoh@csu.fullerton.edu>
2023-04-23 21:18:33 -0700

    Updated main code to work.

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 char** parse(char* s) {
   static char* words[500];
   memset(words, 0, sizeof(words));
-  
+
   char break_chars[] = " \t";
 
   int i = 0;
@@ -25,15 +25,15 @@ char** parse(char* s) {
 }
 
 // Main Function
-int main(int argc, const char * argv[]) {  
+int main(int argc, const char * argv[]) {
   char input[BUFSIZ];
   char last_command[BUFSIZ];
   int i;
-  
+
   memset(input, 0, BUFSIZ * sizeof(char));
   memset(input, 0, BUFSIZ * sizeof(char));
   bool finished = false;
-  
+
   // Start of main while loop
   while (!finished) {
     printf("osh> ");                                // Prompting user
@@ -47,47 +47,49 @@ int main(int argc, const char * argv[]) {
     printf("input was: \n'%s'\n", input);
 
     // History section of main while loop
-    if (strncmp(input, "!!", 2) == 0) {            
+    if (strncmp(input, "!!", 2) == 0) {
       if (strlen(last_command) == 0) {
         fprintf(stderr, "no last command to execute\n");
       }
       printf("last command was: %s\n", last_command);
-    } else if (strncmp(input, "exit", 4) == 0) {   
+    } else if (strncmp(input, "exit", 4) == 0) {
       finished = true;
     } else {
       strcpy(last_command, input);
-      printf("You entered: %s\n", input); 
+      printf("You entered: %s\n", input);
 
       // Forking Child Process section of main while loop
       char** words = parse(input);
-      pid_t pid = fork();  
-      if (pid < 0) { 
-        printf("\nFailed forking child.."); 
-        return 0; 
-      } else if (pid == 0) { 
-
-          // For-loop rough draft for Redirection checking (COULD NOT GET WORKING)
-         /* char** words = parse(input);
-          for (int i = 0; words[i] != NULL; i++) {
-             if (strcmp("<", words[i])) {
-              int fd0 = open(words[strlen(*words)-1], O_RDONLY);
-              dup2(fd0, STDIN_FILENO);
-              close(fd0);
-            } else if (strcmp(">", words[i])) {
-              int fd1 = creat(last_command, 0644);
-              dup2(fd1, STDOUT_FILENO);
-              close(fd1);
-            }
-          } */
-
+      pid_t pid = fork();
+      if (pid < 0) {
+        printf("\nFailed forking child..");
+        return 0;
+      } else if (pid == 0) {
+        // Redirection checking
+        int fd_in = 0;
+        int fd_out = 1;
+        for (int i = 0; words[i] != NULL; i++) {
+          if (strcmp(words[i], "<") == 0) {
+            fd_in = open(words[i+1], O_RDONLY);
+            dup2(fd_in, STDIN_FILENO);
+            close(fd_in);
+            words[i] = NULL;
+            words[i+1] = NULL;
+          } else if (strcmp(words[i], ">") == 0) {
+            fd_out = creat(words[i+1], 0644);
+            dup2(fd_out, STDOUT_FILENO);
+            close(fd_out);
+            words[i] = NULL;
+            words[i+1] = NULL;
+          }
+        }
         // Invoking execvp in child process after Redirection
         execvp(words[0],words);
+        printf("\n");
+        return 0;
 
-  printf("\n");
-  return 0;
-
-      } else { 
-         wait(NULL);                             // Waiting for child to terminate 
+      } else {
+        wait(NULL);                             // Waiting for child to terminate
       }
       printf("\n");
     }
@@ -95,6 +97,6 @@ int main(int argc, const char * argv[]) {
 
   printf("osh exited\n");
   printf("program finished\n");
-  
+
   return 0;
 }
